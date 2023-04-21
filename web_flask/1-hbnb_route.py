f6b792b4fd4e42afcc18a9e766e5e88e23dc716a
Xys-Tus <Sixtus Onyedibe>
2023-04-21 21:39:44 +0100

    Refactored my code to adhere with pep8 style

--- web_flask/1-hbnb_route.py
+++ web_flask/1-hbnb_route.py
@@ -6,15 +6,18 @@ Working with Flask
 from flask import Flask
 app = Flask(__name__)
 
+
 @app.route('/', strict_slashes=False)
 def hello_HBNB():
     """returns Hello HBNB!"""
     return 'Hello HBNB!'
 
+
 @app.route('/hbnb', strict_slashes=False)
 def to_hbnb():
     """returns HBNB"""
-    return 'HBNB!'
+    return 'HBNB'
+
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port='5000')
