e51b151a2c8eb6b71c8eb73d1ea9caa4ae0065ad
Xys-Tus <Sixtus Onyedibe>
2023-04-22 00:08:46 +0100

    Is a number

---
+++ web_flask/4-number_route.py
@@ -0,0 +1,44 @@
+#!/usr/bin/python3
+"""
+Working with Flask
+displays a defualt if nothing is provided
+"""
+
+from flask import Flask
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def hello_HBNB():
+    """returns Hello HBNB!"""
+    return 'Hello HBNB!'
+
+
+@app.route('/hbnb', strict_slashes=False)
+def to_hbnb():
+    """returns HBNB"""
+    return 'HBNB'
+
+
+@app.route('/c/<string:text>', strict_slashes=False)
+def to_nextpage(text):
+    """returns HBNB"""
+    return 'C ' + text.replace('_', ' ')
+
+
+@app.route('/python', strict_slashes=False)
+@app.route('/python/<text>', strict_slashes=False)
+def to_python_text(text='is cool'):
+    """returns Python is cool if nothing is passed
+        else return python followed by the world that was passed
+    """
+    return 'Python ' + text.replace('_', ' ')
+
+
+@app.route('/c/<int:n>', strict_slashes=False)
+def to_isNum(n):
+    """returns HBNB"""
+    return '{} is a numbet'.format(n)
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port='5000')
