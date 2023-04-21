c1cc653c10e3499bab86eebd8912fe315a92a0e0
Xys-Tus <Sixtus Onyedibe>
2023-04-21 20:31:30 +0100

    Implemented routing

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -2,16 +2,15 @@
 """Working with Flask framework
 This app listens on 0.0.0.0, port 5000.
 """
-
 from flask import Flask
 
 app = Flask(__name__)
 
-@app.route('/', strict_slashes=False)
+@app.route("/", strict_slashes=False)
 def hello_HBNB():
     """returns Hello HBNB!"""
-    return 'Hello HBNB!'
+    return "Hello HBNB!"
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     app.run(host='0.0.0.0', port='5000')
