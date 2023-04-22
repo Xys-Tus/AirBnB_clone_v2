86bf6f91713f76b11135c9ef2d50bec919dc3806
Xys-Tus <Sixtus Onyedibe>
2023-04-22 22:54:09 +0100

    Render's a html file passed a number

--- web_flask/5-number_template.py
+++ web_flask/5-number_template.py
@@ -41,7 +41,7 @@ def to_isNum(n):
     return '{} is a number'.format(n)
 
 
-@app.route('/number_template/<n>', strict_slashes=False)
+@app.route('/number_template/<int:n>', strict_slashes=False)
 def to_num_tem(n):
     """Render's a static file"""
     return render_template('5-number.html', n=n)
