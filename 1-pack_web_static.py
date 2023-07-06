13d694b99ac476f2e8c5fc5b8a6845bbefea1a5d
Xys-Tus <Sixtus Onyedibe>
2023-07-06 19:08:27 +0100

    Working with Fabric

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -7,16 +7,17 @@ folder of the AirBnB Clone repo
 from datetime import datetime
 from fabric.api import local
 from os.path import isdir
+import os
 
 
 def do_pack():
     """generates a tgz archive"""
-    try:
-        date = datetime.now().strftime("%Y%m%d%H%M%S")
-        if isdir("versions") is False:
-            local("mkdir versions")
-        file_name = "versions/web_static_{}.tgz".format(date)
-        local("tar -cvzf {} web_static".format(file_name))
-        return file_name
-    except:
-        return None
+    path = "./web_static"
+    current_dataNtime = datetime.today().strftime("%Y%m%d%H%M%S")
+    if os.path.isdir(path):
+        for root, dirs, files in os.walk(path):
+            for file in files:
+                local(f"tar -czvf web_static{current_dataNtime}.tar.gz {file}")
+                print(f"./web_static/{file}")
+    else:
+        print("Not a directory")
